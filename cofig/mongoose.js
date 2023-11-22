b9dab512bc6e85b3f9f237922fd13a6cbf898c24
RNW <janakkhokhar28@gmail.com>
2023-11-22 14:25:06 +0530

    Change Live Cluster mongo

--- cofig/mongoose.js
+++ cofig/mongoose.js
@@ -1,14 +1,14 @@
-let mongoose = require('mongoose');
+// let mongoose = require('mongoose');
 
-mongoose.connect("mongodb://127.0.0.1/batch");
+// mongoose.connect("mongodb://127.0.0.1/batch");
 
-let db  = mongoose.connection;
+// let db  = mongoose.connection;
 
-db.once('open',function(err){
-    if(err){
-        console.log("db not connected");
-    }
-    console.log("db is connected");
-});
+// db.once('open',function(err){
+//     if(err){
+//         console.log("db not connected");
+//     }
+//     console.log("db is connected");
+// });
 
-module.exports= db;
+// module.exports= db;
--- index.js
+++ index.js
@@ -1,28 +1,37 @@
-let express  = require('express');
+let express = require("express");
 let port = 8004;
 let app = express();
-let path = require('path');
-let fs = require('fs');
+let path = require("path");
+let fs = require("fs");
 
-let db = require('./cofig/mongoose');
-let Student = require('./models/Student');
+// let db = require('./cofig/mongoose');
 
-app.set('view engine','ejs');
-app.set('views',path.join(__dirname,'views'));
+let mongoose = require("mongoose");
+const connectionString =
+  "mongodb+srv://janak_khokhar:janak123456@cluster0.3kzcmtj.mongodb.net/batch";
+mongoose
+  .connect(connectionString, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("Database connected."))
+  .catch((err) => console.log(err));
+let Student = require("./models/Student");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded());
 
 // image
-app.use('/upload',express.static(path.join(__dirname,'upload')));
+app.use("/upload", express.static(path.join(__dirname, "upload")));
 // end
 
+app.use("/", require("./routes"));
+app.use("/post", require("./routes/post"));
 
-
-app.use('/',require('./routes'));
-app.use('/post',require('./routes/post'));
-
-app.listen(port , function(err){
-    if(err){
-        console.log('db not conntect');
-    }
-    console.log('db running sunccess');
-})
+app.listen(port, function (err) {
+  if (err) {
+    console.log("db not conntect");
+  }
+  console.log("db running sunccess");
+});
